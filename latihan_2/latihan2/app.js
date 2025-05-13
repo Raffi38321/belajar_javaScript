c0ede202272734e49836f88353bda43418c6d2c1
Raffi <raffi38321@gmail.com>
2025-05-13 16:46:00 +0700

    belajar dom 1

---
+++ latihan_2/latihan2/app.js
@@ -0,0 +1,13 @@
+// 1. Select element h4 which holds text of "Red" by using getElementsByTagName
+// 2. Select div with the class of "green" which holds text (Green) by using getElementsByTagName
+// 3. Select div with the ID of blue which holds the text "Blue" by using getElementsByClassName
+// 4. Select div which has the class & Id of "yello" by using querySelector()
+// 5. Select all the elements which has the class of "teal" by using querySelectorAll
+
+var merah = document.getElementsByName("h4")
+var hijau = document.getElementsByClassName("green")
+var biru =  document.getElementById("blue")
+var kuning = document.querySelector(".yellow")
+var teal = document.querySelectorAll("teal")
+
+//8:18
